2cb6ef71c162c7fedbed7407fdadbe3be40aa1d6
Venux <arifmp40@gmail.com>
2023-07-17 01:02:54 +0600

    asda

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACK_END_URL,
   withCredentials: true,
   headers: {
-    "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_BASE_BACK_END_URL,
+    "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_BACK_END_BASE_URL,
   },
 });
 
