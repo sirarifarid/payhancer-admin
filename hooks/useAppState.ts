c33f843da00ff9fa08c31e81aaf47388956e7eb5
Venux <arifmp40@gmail.com>
2023-07-17 02:13:36 +0600

    asda

--- hooks/useAppState.ts
+++ hooks/useAppState.ts
@@ -1,5 +1,6 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
+import { useLayoutEffect } from "react";
 
 export function useAppState<T>(
   key: string,
@@ -12,13 +13,13 @@ export function useAppState<T>(
     ...p_options,
   };
   const { enabled, initial } = options || {};
+  useLayoutEffect(() => {}, []);
   function setQuery(updater: any) {
-    queryClient.setQueryData([key + "-cache"], {
-      ...queryClient.getQueryData<T>([key]),
-      ...updater,
-    });
+    queryClient.setQueryData([key + "-cache"], updater);
   }
-
+  useLayoutEffect(() => {}, [
+    queryClient.getQueryData<AxiosResponse<T>>([key])?.data,
+  ]);
   const data = useQuery<T>({
     queryKey: [key + "-cache"],
     queryFn: () =>
